a5aaf58c7280c7f322d076e7a1dfd7a4a24134d9
Nubia N. Hermes <nuuhermes456@gmail.com>
2022-03-31 16:02:47 -0300

    atualizacoes

--- js/cadastro.js
+++ js/cadastro.js
@@ -1,4 +1,5 @@
-// Fazendo Script para os olhinhos de senha ( 22/ mar /2022 )
+
+"use scrict"
 
 const senhaCadastro = document.querySelector('.senhaCadastro');
 const senhaCadastro2 = document.querySelector('.senhaCadastro2');
@@ -21,5 +22,54 @@ btnOlho2.onclick = () => {
     } else {
         senhaCadastro2.type = 'text'
         btnOlho2.src = 'img/MostrarSenha.svg'
+    }
+}
+
+// barra de forca da senha
+
+function validarSenhaForca() {
+    var senha = document.getElementById('senhaForca').value;
+    var forca = 0;
+    // document.getElementById('impSenha').innerHTML = "Senha: " + senha;
+
+    if((senha.length >= 4) && (senha.length <= 7)){
+        forca += 10;
+    }else if(senha.length > 7){
+        forca += 25;
+    }
+
+    if((senha.length >= 5) && (senha.match(/[a-z]+/))){
+        forca += 10;
+    }
+
+    if((senha.length >= 6) && (senha.match(/[A-Z]/))){
+        forca += 20;
+    }
+
+    if((senha.length >=7) && (senha.match(/[@#$%&*!;]/))){
+        forca += 25
+    }
+
+    if(senha.match(/([1-9]+)\1{1,}/)){
+		forca += -25;
+	}
+
+     mostrarForca(forca)
+     console.log(mostrarForca);
+
+}
+
+function mostrarForca(forca){
+    /*imprimir a forca*/
+   //document.getElementById("impForcaSenha").innerHTML = "Forca: " + forca;
+
+    if(forca === 0 ){
+        document.getElementById("erroSenhaForca").innerHTML = '<div class="progress"><div class="progress-bar bg-light" role="progressbar" style="width: 0%" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div></div>';
+    } else if((forca > 0) && (forca < 40)){
+        document.getElementById("erroSenhaForca").innerHTML = '<div class="progress"><div class="progress-bar bg-danger" role="progressbar" style="width: 10%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div></div>';
+    } else if ((forca >= 40) && (forca < 70)){
+        document.getElementById("erroSenhaForca").innerHTML =  '<div class="progress"><div class="progress-bar bg-warning" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div></div>';
+    } else if ((forca >= 70) && (forca < 100)){
+        document.getElementById("erroSenhaForca").innerHTML =  '<div class="progress"><div class="progress-bar bg-success" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div></div>';
     }
 }
--- js/forcaSenha.js
+++ js/forcaSenha.js
@@ -1,44 +1,44 @@
 mostrarForca(0);
 
-function validarSenhaForca(){
+function validarSenhaForca() {
     var senha = document.getElementById('senhaForca').value;
     var forca = 0;
 
-     /* imprimir a senha */
+    /* imprimir a senha */
 
     /*document.getElementById("impSenha").innerHTML = "Senha: " + senha;*/
 
-    if((senha.length >= 4) && (senha.length <= 7)){
+    if ((senha.length >= 4) && (senha.length <= 7)) {
         forca += 10;
-    }else if(senha.length > 7){
+    } else if (senha.length > 7) {
         forca += 25;
     }
 
-    if((senha.length >= 5) && (senha.match(/[a-z]+/))){
+    if ((senha.length >= 5) && (senha.match(/[a-z]+/))) {
         forca += 10;
     }
 
-    if((senha.length >= 6) && (senha.match(/[A-Z]+/))){
+    if ((senha.length >= 6) && (senha.match(/[A-Z]+/))) {
         forca += 20;
     }
 
-    if((senha.length >= 7) && (senha.match(/[@#$%&*;!]+/))){
+    if ((senha.length >= 7) && (senha.match(/[@#$%&*;!]+/))) {
         forca += 25;
     }
-    
+
     mostrarForca(forca);
 }
 
-function mostrarForca(forca){
+function mostrarForca(forca) {
     /*imprimir a forca*/
-   /* document.getElementById("impForcaSenha").innerHTML = "Forca: " + forca;*/
-   if(forca === 0){
-    document.getElementById("erroSenhaForca").innerHTML = '<div class="progress"><div class="progress-bar bg-light" role="progressbar" style="width: 0%" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div></div>'; 
-   }else if((forca > 0) && (forca < 40)){
-        document.getElementById("erroSenhaForca").innerHTML = '<div class="progress"><div class="progress-bar bg-danger" role="progressbar" style="width: 10%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div></div>'; 
-    }else if((forca >= 40) && (forca < 70)){
-        document.getElementById("erroSenhaForca").innerHTML = '<div class="progress"><div class="progress-bar bg-warning" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div></div>'; 
-    }else if((forca >= 70) && (forca < 100)){
-        document.getElementById("erroSenhaForca").innerHTML = '<div class="progress"><div class="progress-bar bg-success" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div></div>'; 
-    } 
+    /* document.getElementById("impForcaSenha").innerHTML = "Forca: " + forca;*/
+    if (forca === 0) {
+        document.getElementById("erroSenhaForca").innerHTML = '<div class="progress"><div class="progress-bar bg-light" role="progressbar" style="width: 0%" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div></div>';
+    } else if ((forca > 0) && (forca < 40)) {
+        document.getElementById("erroSenhaForca").innerHTML = '<div class="progress"><div class="progress-bar bg-danger" role="progressbar" style="width: 10%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div></div>';
+    } else if ((forca >= 40) && (forca < 70)) {
+        document.getElementById("erroSenhaForca").innerHTML = '<div class="progress"><div class="progress-bar bg-warning" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div></div>';
+    } else if ((forca >= 70) && (forca < 100)) {
+        document.getElementById("erroSenhaForca").innerHTML = '<div class="progress"><div class="progress-bar bg-success" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div></div>';
+    }
 }
