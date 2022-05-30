4d625bae19a6195bc66654127d77b28a8915ae78
Mateus Bispo Ramos <niremalu.mateus@gmail.com>
2022-05-30 16:55:30 -0300

    modal de redefinir senha finalizado

--- js/redefinirSenha.js
+++ js/redefinirSenha.js
@@ -2,32 +2,36 @@ const modalRedefinirSenha = () => {
 	
 	conteudoModal(`  
     <link rel="stylesheet" type="text/css" href="../style/redefinirSenha.css">
+    
+
     <div id="tituloSenha">
     <div class="editarTitulo">Redifinir Senha</div>
 </div>
 <div id="paiDasCaixas">
 <div id="caixasSenha">
     <div id="senhaAtual">Senha atual</div>
-    <div id="caixaSenhaAtual">digite sua senha</div>
+    <input id="caixaSenhaAtual" placeholder="digite sua senha"></input>
 </div>
 
 <div id="caixasSenha">
     <div id="senhaAtual">Nova senha</div>
-    <div id="caixaSenhaAtual">digite sua senha</div>
+    <input id="caixaSenhaAtual" placeholder="digite sua senha"></input>
 </div>
 
 <div id="caixasSenha">
     <div id="senhaAtual">Confirme sua senha</div>
-    <div id="caixaSenhaAtual">****************</div>
+    <input id="caixaSenhaAtual" placeholder="****************"></input>
 </div>
 </div>
 
-<div id="buttonRedefinirSenha">
+<div id="editarButton">
+<div id="buttonRedefinirSenha" type="button">
 <div>Redefinir</div>
 </div>
 <div class="editarButtonCancelar">
 <div id="editarTagA">Cancelar</div>
-</div> `);
+</div> 
+</div>`);
 
 var modal = document.getElementById('modalUsuario');
 modal.style.display = 'none';
